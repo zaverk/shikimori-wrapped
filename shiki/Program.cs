388efc3cf23b496645c650dd1f6fcab2b183b18e
zaverk <dot@DESKTOP-B8J6DDE>
2021-12-14 13:34:58 +0300

    statements refactoring

--- shiki/Program.cs
+++ shiki/Program.cs
@@ -14,50 +14,47 @@ namespace shiki
         public string Created_at { get; set; }
         public static void Main()
         {
-            using (var w = new WebClient())
+            using var w = new WebClient();
+            var json_data = string.Empty;
+            string? userid = Console.ReadLine();
+            bool s = true;
+            string currentyear = DateTime.Now.Year.ToString();
+            int q = 0; // will not be used
+            int e = 0;
+            // attempt to download JSON data as a string
+            for (int i = 1; s == true; i++)
             {
-                var json_data = string.Empty;
-                string? userid = Console.ReadLine();
-                bool s = true;
-                string currentyear = DateTime.Now.Year.ToString();
-                int q = 0;
-                int e = 0;
-                // attempt to download JSON data as a string
-                for (int i = 1; s == true; i++)
+                try
                 {
-                    try
-                    {
-                        json_data = w.DownloadString($"https://shikimori.one/api/users/{userid}/history?page={i}&limit=100&target_type=Anime");
-                        if (json_data == null || json_data == "[]")
-                        {
-                            s = false;
-                            break;
-                        }
-                        var response = JArray.Parse(json_data);
-                        foreach (JObject item in response)
-                        {
-                            string name = item.GetValue("target").ElementAt(2).First().ToString();
-                            string date = DateTime.Parse(item.GetValue("created_at").ToString()).Year.ToString();
-                            if (date != currentyear)
-                            {
-                                break;
-                            }
-                            string status = item.GetValue("description").ToString();
-                            if (status == "Просмотрено" || status.Contains("Просмотрено и оценено"))
-                            {
-                                Console.WriteLine($"Name: {name} Date: {date} Status: {status}");
-                                e++;
-                            }
-                            q++;
-                        }
-                    }
-                    catch (Exception)
+                    json_data = w.DownloadString($"https://shikimori.one/api/users/{userid}/history?page={i}&limit=100&target_type=Anime");
+                    if (json_data == null)
                     {
                         s = false;
                     }
+                    var response = JArray.Parse(json_data);
+                    foreach (JObject item in response)
+                    {
+                        string name = item.GetValue("target").ElementAt(2).First().ToString();
+                        string date = DateTime.Parse(item.GetValue("created_at").ToString()).Year.ToString();
+                        if (date != currentyear)
+                        {
+                            break;
+                        }
+                        string status = item.GetValue("description").ToString();
+                        if (status == "Просмотрено" || status.Contains("Просмотрено и оценено"))
+                        {
+                            Console.WriteLine($"Name: {name} Date: {date} Status: {status}");
+                            e++;
+                        }
+                        q++;
+                    }
                 }
-                Console.WriteLine($"Всего иттераций: {q} Просмотрено: {e}");
+                catch (Exception)
+                {
+                    s = false;
+                }
             }
+            Console.WriteLine($"Всего иттераций: {q} Просмотрено в этом году: {e}");
         }
     }
 }
